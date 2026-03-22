cf1c3c83cb7ebc9545aa39d9a3a642cd62abc2fd
shweta406 <23cs2042@rgipt.ac.in>
2026-03-22 12:44:14 +0530

    updated data transformation

--- src/textSummarizer/components/data_transformation.py
+++ src/textSummarizer/components/data_transformation.py
@@ -5,6 +5,13 @@ from datasets import load_dataset,load_from_disk
 from textSummarizer.entity import DataTransformationConfig
 
 
+import os
+from textSummarizer.logging import logger
+from transformers import AutoTokenizer
+from datasets import load_dataset,load_from_disk
+from textSummarizer.entity import DataTransformationConfig
+
+
 class DataTransformation:
     def __init__(self, config: DataTransformationConfig):
         self.config = config
@@ -12,18 +19,26 @@ class DataTransformation:
 
 
     
-    def convert_examples_to_features(self,example_batch):
-        input_encodings = self.tokenizer(example_batch['dialogue'] , max_length = 1024, truncation = True )
-        
-        with self.tokenizer.as_target_tokenizer():
-            target_encodings = self.tokenizer(example_batch['summary'], max_length = 128, truncation = True )
-            
+    def convert_examples_to_features(self, example_batch):
+        input_encodings = self.tokenizer(
+            example_batch['dialogue'],
+            max_length=1024,
+            truncation=True,
+            padding="max_length"
+        )
+    
+        target_encodings = self.tokenizer(
+            text_target=example_batch['summary'],
+            max_length=128,
+            truncation=True,
+            padding="max_length"
+        )
+    
         return {
-            'input_ids' : input_encodings['input_ids'],
+            'input_ids': input_encodings['input_ids'],
             'attention_mask': input_encodings['attention_mask'],
             'labels': target_encodings['input_ids']
         }
-    
 
     def convert(self):
         dataset_samsum = load_from_disk(self.config.data_path)
